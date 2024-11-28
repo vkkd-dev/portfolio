f6cfaf1d2e5fb691aaaec4ba95add5d4aa032546
Vargav <vargabdas@gmail.com>
2024-11-28 15:52:29 +0530

    added mobile sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 import Footer from "@/components/Footer";
 import MobileMenu from "@/components/MobileMenu";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Portfolio Website",
@@ -27,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <MobileMenu />
         {children}
         <Footer />
--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,14 +1,64 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Image
-      src="/images/menu.png"
-      width={75}
-      height={75}
-      alt="menu"
-      className="fixed top-12 left-0 z-50"
-    />
+    <div
+      className="bg-primary fixed lg:hidden top-5 left-3 z-50 rounded-full p-1.5"
+      onClick={toggleMenu}
+    >
+      <Image src="/icons/open.svg" width={24} height={24} alt="menu" />
+
+      {/* Full-Screen Menu */}
+      {isOpen && (
+        <div
+          className={`fixed inset-0 bg-primary z-40 flex flex-col justify-center items-center space-y-6 
+          transform transition-transform duration-500 ${
+            isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+          }`}
+        >
+          {/* Close Button */}
+
+          <div className="bg-white fixed lg:hidden top-5 left-3 z-50 rounded-full p-1.5">
+            <Image
+              src="/icons/close.svg"
+              width={24}
+              height={24}
+              alt="menu"
+              onClick={toggleMenu}
+            />
+          </div>
+
+          {/* Menu Items */}
+          <nav className="flex flex-col items-center text-white text-2xl font-semibold space-y-10">
+            <Link href="#" className="hover:text-gray-300">
+              Home
+            </Link>
+            <Link href="#" className="hover:text-gray-300">
+              About
+            </Link>
+            <Link href="#" className="hover:text-gray-300">
+              Services
+            </Link>
+            <Link href="#" className="hover:text-gray-300">
+              Portfolio
+            </Link>
+            <Link href="#" className="hover:text-gray-300">
+              Contact
+            </Link>
+          </nav>
+        </div>
+      )}
+    </div>
   );
 };
 
